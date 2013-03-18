4f3017b0985750740b2f0df23f56f497437d2f1b
Eddy Davila <EdDavila@fullsail.edu>
2013-03-18 12:11:04 -0400

    Added boolean to .js file

--- davila_eddy_assignment3.js
+++ davila_eddy_assignment3.js
@@ -17,5 +17,26 @@ console.log ("I play guitar twice a month in " + myHome + "; during summer and w
 console.log ("I have " + myNumber + " guitars");
 console.log ("Believe it or not, its " + hasGuitar + " that I have " + myNumber + " guitars.");
 
+var seasons = function(summer, winter){
+var whichSeason = summer + winter;
+console.log("I will perform " + whichSeason + " sets; " + setTime + " minutes each.");
+};
+seasons(1,1); 
 
 
+// Conditional
+
+if (hasGuitar === true) {
+   if (myNumber <= 222) { 
+      myNumber = myNumber + 10;
+      console.log( "If I had " + myNumber + ", I'll become the best");
+    } else { 
+      console.log("I won't become the best if I have any less than" + myNumber);
+    }
+} else {
+      if (myName === "Ed Davila") { 
+         console.log("My name is " + myName + ", and I have guitars")
+      } else { 
+        console.log("My name is " + myName + ", and I do not have guitars");
+		}
+};
