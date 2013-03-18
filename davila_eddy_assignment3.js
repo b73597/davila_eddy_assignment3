bebd5dd4643a696d84f5039ee929995d42d8a2eb
Eddy Davila <EdDavila@fullsail.edu>
2013-03-18 11:46:35 -0400

    Added first files to project3 repo

---
+++ davila_eddy_assignment3.js
@@ -0,0 +1,21 @@
+// Eddy O. Davila
+// SDI 1303
+// Project 3
+
+//Ed Davila The RockStar
+
+// my variables
+//variables
+var myName = "Ed Davila";
+var myHome = "Cuenca";
+var myNumber = 222;
+var hasGuitar = true;
+var setTime = 30;
+
+console.log ("My name is " + myName);
+console.log ("I play guitar twice a month in " + myHome + "; during summer and winter");
+console.log ("I have " + myNumber + " guitars");
+console.log ("Believe it or not, its " + hasGuitar + " that I have " + myNumber + " guitars.");
+
+
+
